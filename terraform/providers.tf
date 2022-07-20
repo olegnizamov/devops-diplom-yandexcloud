293376a493606c5f9da31ce69e5581a00af45338
Олег Низамов <35757313+olegnizamov@users.noreply.github.com>
2022-07-20 17:05:10 +0400

    Диплом

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -3,25 +3,25 @@ terraform {
     yandex = {
       source = "yandex-cloud/yandex"
     }
-   }
+  }
 
 
-backend "s3" {
+  backend "s3" {
     endpoint   = "storage.yandexcloud.net"
     bucket     = "diplom-olegnizamov"
     region     = "ru-central1"
     key        = "./prod/state.tfstate"
-    access_key = "YCAJE5ieunzKNMjNOLY0sHgpo"
-    secret_key = "YCN1oDde4PFkv8CPsMH6NOHSpiNMVkUZLUPc_MqH"
+    access_key = "*********"
+    secret_key = "*********"
 
 
     skip_region_validation      = true
     skip_credentials_validation = true
- }
+  }
 }
 
 provider "yandex" {
-  token     = "AQAAAAAK95aFAATuwVVwsVDzcENouB59H-1n21E"
+  token     = "*********"
   cloud_id                 = "b1gtitubqcoaoesmi8vd"
   folder_id                = "b1gvcj5c7qi25j81c8ob"
   zone      = "ru-central1-a"
